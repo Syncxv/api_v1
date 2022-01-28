3e45b3da2578817ae78db6bee1884ab3c4007ee0
Syncxv <aruldevarackal1@gmail.com>
2022-01-28 08:58:33 +0530

    add users and remove them

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,11 @@
 import { prop, getModelForClass } from '@typegoose/typegoose'
 import { Field, ObjectType } from 'type-graphql'
+
 @ObjectType()
 export class UserClass {
+    @Field()
+    readonly _id: string
+
     @prop({ required: true })
     @Field()
     public username: string
@@ -11,14 +15,11 @@ export class UserClass {
     public email: string
 
     @prop({ required: true, select: false })
-    @Field()
     private password: string
 
-    @prop()
-    @Field()
+    @prop({ required: false })
+    @Field({ nullable: true })
     public avatar?: string
 }
 
 export const UserModel = getModelForClass(UserClass)
-
-// "document" has proper types of KittenClass
--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -1,11 +1,47 @@
-import { Query, Resolver } from 'type-graphql'
+import { Field, InputType, Mutation, Query, Resolver } from 'type-graphql'
 import { UserModel } from '../models'
 import { UserClass } from '../models/User'
+import { Arg } from 'type-graphql'
 
-@Resolver(of => UserModel)
+@InputType()
+class UserCreateTempArgs {
+    @Field()
+    username: string
+    @Field()
+    password: string
+    @Field()
+    email: string
+    @Field({ nullable: true })
+    avatar?: string
+}
+
+@Resolver(_ => UserModel)
 export class userReslover {
     @Query(() => [UserClass])
     async getUsers(): Promise<UserClass[]> {
         return await UserModel.find()
+    }
+    @Mutation(() => UserClass)
+    async createUserTemp(
+        @Arg('options') options: UserCreateTempArgs
+    ): Promise<UserClass> {
+        const { username, email, password, avatar } = options
+        const user = await UserModel.create({
+            username,
+            email,
+            password,
+            avatar
+        })
+        console.log(user)
+        return user
+    }
+    @Mutation(() => Boolean)
+    async removeUserTemp(@Arg('id') id: string): Promise<boolean> {
+        const user = await UserModel.findById(id)
+        if (user) {
+            await user.delete()
+            return true
+        }
+        return false
     }
 }
