2107fedc5723d9548c32ce5892b1ea58a0e584b5
Syncxv <aruldevarackal1@gmail.com>
2022-02-02 07:52:57 +0530

    ah i didnt save

--- src/types.ts
+++ src/types.ts
@@ -13,19 +13,3 @@ export enum Feilds {
     EMAIL = 'email',
     UNKOWN = 'unkown'
 }
-
-export const whitelist = [
-    'http://localhost:3000',
-    'https://studio.apollographql.com',
-    '*'
-]
-export const corsOptions = {
-    origin: function (origin: any, callback: any) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true
-}
