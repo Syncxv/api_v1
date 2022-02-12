c9aa84232a217be6f04a8801d33f1b56874d8c9f
Syncxv <aruldevarackal1@gmail.com>
2022-02-12 19:46:38 +0530

    expire time from 60m to 10h :|

--- src/jwt/index.ts
+++ src/jwt/index.ts
@@ -3,7 +3,7 @@ import { UserClass } from '../models/User'
 
 export const createAcessToken = (user: UserClass) => {
     return sign({ user }, process.env.ACCESS_TOKEN_SECRET!, {
-        expiresIn: '60m'
+        expiresIn: '10h'
     })
 }
 
