7567df519552fccde4f1949cbda05110b2802509
Syncxv <aruldevarackal1@gmail.com>
2022-02-25 21:01:37 +0530

    constants and whoopsie forgot to commit yarnlock

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,12 @@
 export const whitelist = [
     'http://localhost:3000',
     'https://studio.apollographql.com',
-    '*'
+    process.env.FRONT_END_DOMAIN!
 ]
+export const COOKIE_NAME = 'token'
 export const corsOptions = {
     origin: function (origin: any, callback: any) {
-        if (whitelist.indexOf(origin) !== -1) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
@@ -13,6 +14,9 @@ export const corsOptions = {
     },
     credentials: true
 }
-
 export const apiUrl = process.env.APIRUL || 'http://localhost:8000'
 export const defaultLimit = 10
+export const JWT_AGE = {
+    string: '10h',
+    int: 3.6e7
+}
