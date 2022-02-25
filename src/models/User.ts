31038fb8f4dcea24ebbbf99a1b5fbb07f2c3d20d
Syncxv <aruldevarackal1@gmail.com>
2022-02-25 09:46:17 +0530

    added populate methods to user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,13 @@
-import { prop, getModelForClass, Ref, pre } from '@typegoose/typegoose'
+import {
+    prop,
+    getModelForClass,
+    Ref,
+    pre,
+    ReturnModelType
+} from '@typegoose/typegoose'
 import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
 import { Field, ObjectType } from 'type-graphql'
+import { MongoDocument } from '../types'
 
 @pre<UserClass>('validate', function () {
     if (!this.displayName) {
@@ -37,13 +44,49 @@ export class UserClass extends TimeStamps {
     @Field({ nullable: true })
     public avatar?: string
 
-    @prop({ ref: () => UserClass, default: [] })
+    @prop({
+        ref: () => UserClass,
+        default: [] /** options: {limit: 10} 
+                DOES THis LIMIT HOW MUCH I CAN PUT IN IT OR DOES IT LIMIT HOW MUCH GETS SENT BACK?!?!?!?!?!?!?! 
+                ill figure this out later because i cant be botherd to make 10 accounts
+            */
+    })
     @Field(() => [Follower])
     public followers: Ref<UserClass>[]
 
     @prop({ required: false, default: false })
     @Field()
     public isStaff: boolean
+
+    public static async findAndPopulate(
+        this: ReturnModelType<typeof UserClass>,
+        query?: any
+    ) {
+        return await this.find(query || {}).populate({
+            path: 'followers',
+            select: ['username', 'id']
+        })
+    }
+
+    public static async findByIdAndPopulate(
+        this: ReturnModelType<typeof UserClass>,
+        id: string
+    ) {
+        return this.findById(id).populate({
+            path: 'followers',
+            select: ['username', 'id']
+        })
+    }
+
+    public static async populateModel(
+        this: ReturnModelType<typeof UserClass>,
+        post: MongoDocument<UserClass>
+    ) {
+        return await post.populate({
+            path: 'followers',
+            select: ['username', 'id']
+        })
+    }
 }
 
 export const UserModel = getModelForClass(UserClass)
