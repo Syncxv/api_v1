e3c96a6f59f3a8d5286cc3ca9bb3151b809720fa
Syncxv <aruldevarackal1@gmail.com>
2022-02-25 13:24:43 +0530

    damn how did i not see this before
    i was sending passwords with the jwt AND ALSO it was a mongoose object so it had so many things that we didnt need like isnew and init :|

--- src/jwt/index.ts
+++ src/jwt/index.ts
@@ -1,14 +1,23 @@
 import { sign } from 'jsonwebtoken'
 import { UserClass } from '../models/User'
+import { MongoDocument } from '../types'
 
-export const createAcessToken = (user: UserClass) => {
-    return sign({ user }, process.env.ACCESS_TOKEN_SECRET!, {
-        expiresIn: '10h'
-    })
+export const createAcessToken = (user: MongoDocument<UserClass>) => {
+    return sign(
+        { user: { ...user.toJSON(), password: undefined } },
+        process.env.ACCESS_TOKEN_SECRET!,
+        {
+            expiresIn: '10h'
+        }
+    )
 }
 
-export const createRefreshToken = (user: UserClass) => {
-    return sign({ user }, process.env.REFRESH_TOKEN_SECRET!, {
-        expiresIn: '7d'
-    })
+export const createRefreshToken = (user: MongoDocument<UserClass>) => {
+    return sign(
+        { user: { ...user.toJSON(), password: undefined } },
+        process.env.REFRESH_TOKEN_SECRET!,
+        {
+            expiresIn: '7d'
+        }
+    )
 }
